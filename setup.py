d4ff2b7d96ac56f52ce22699d2eb196b695e02c5
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2023-04-17 12:41:22 +0530

    Project

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='qr-filetransfer',
+    version='2.6.1',
+    author='Ayush Saini',
+    author_email='ayushsaini469@gmail.com',
+    description='Transfer files over WiFi between your computer and your smartphone from the terminal',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['qr-filetransfer = qr_filetransfer:main']},
+    install_requires=['qrcode', 'colorama; platform_system == "Windows"'],
+    extras_require={'extras': ['netifaces']}
+)
